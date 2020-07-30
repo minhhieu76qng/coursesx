85f4d90ff55a5736d5a33b7c5ac49f4398de0d2e
Hieu Do <minhhieu76qng@gmail.com>
2020-07-31 01:11:40 +0700

    create error video component

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,9 +1,12 @@
 import React, { useMemo, useEffect, useState, useContext } from 'react';
 import YoutubePlayer, { getYoutubeMeta } from 'react-native-youtube-iframe';
-// import { Video as ExpoVideo } from 'expo-av';
+import { useTheme } from '@react-navigation/native';
+import { Video as ExpoVideo } from 'expo-av';
 import { View } from 'react-native';
 import Icon from 'themes/Icon';
+import Text from 'components/Text';
 import { VIDEO_TYPE } from '../../constants';
+import styles from './styles';
 
 const VideoPlayerContext = React.createContext({
   isLoading: false,
@@ -15,22 +18,30 @@ const VideoPlayerContext = React.createContext({
   isYoutubeVideo: true,
 });
 
-// const WrongVideoUI = () => {
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <Text>Bài học bị lỗi.</Text>
-//     </View>
-//   );
-// };
+const WrongVideoUI = () => {
+  const { isYoutubeVideo } = useContext(VideoPlayerContext);
+  const iconName = useMemo(() => (isYoutubeVideo ? 'youtube' : 'film'), [isYoutubeVideo]);
+  return (
+    <View style={styles.otherComps}>
+      <Icon name={iconName} size={50} />
+      <Text style={{ marginTop: 20 }}>Video link is wrong. Please contact admin to resolve!</Text>
+    </View>
+  );
+};
 
 const LoadingSkeleton = () => {
   const { isYoutubeVideo } = useContext(VideoPlayerContext);
   const iconName = useMemo(() => (isYoutubeVideo ? 'youtube' : 'film'), [isYoutubeVideo]);
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={styles.otherComps}>
       <Icon name={iconName} size={50} />
     </View>
   );
+};
+
+const ExpoVideoPlayer = () => {
+  const { videoUrl } = useContext(VideoPlayerContext);
+  return <ExpoVideo source={{ uri: videoUrl }} />;
 };
 
 const YoutubeVideoPlayer = () => {
@@ -58,6 +69,7 @@ const YoutubeVideoPlayer = () => {
 const VideoPlayer = ({ courseData, playingLesson }) => {
   const [isLoading, setLoading] = useState(true);
   const [height, setHeight] = useState(250);
+  const { colors } = useTheme();
 
   const isYoutubeVideo = useMemo(() => courseData?.typeUploadVideoLesson === VIDEO_TYPE.YOUTUBE, [
     courseData,
@@ -74,7 +86,10 @@ const VideoPlayer = ({ courseData, playingLesson }) => {
         isYoutubeVideo,
       }}
     >
-      {isYoutubeVideo ? <YoutubeVideoPlayer /> : null}
+      <View style={[styles.container, { height, backgroundColor: colors.card }]}>
+        {!playingLesson?.videoUrl && <WrongVideoUI />}
+        {playingLesson?.videoUrl && (isYoutubeVideo ? <YoutubeVideoPlayer /> : <ExpoVideoPlayer />)}
+      </View>
     </VideoPlayerContext.Provider>
   );
 };
--- src/components/VideoPlayer/styles.js
+++ src/components/VideoPlayer/styles.js
@@ -2,6 +2,12 @@ const { StyleSheet } = require('react-native');
 
 export default StyleSheet.create({
   container: {
+    // flex: 1,
+    width: '100%',
+  },
+  otherComps: {
+    justifyContent: 'center',
+    alignItems: 'center',
     flex: 1,
   },
 });
